0c81ca444d69560a7e14664dee3b40e4a199af72
Peter Mujuni <kontakt@petermujuni.dk>
2021-12-10 11:59:12 +0100

    add Global styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import { ThemeProvider, createGlobalStyle } from 'styled-components';
+import logo from './logo.svg'
 import './App.css';
+import StyledButton, { FancyButton, SubmitButtom } from './components/Button/Button';
+import { AnimatedLogo, DarkButton } from './components/Button/Button.styles';
+
+const theme = {
+  dark: {
+    primary: 'black',
+    text: 'white'
+  },
+  light: {
+    primary: 'white',
+    text: 'black'
+  }
+}
+
+const GlobalStyle = createGlobalStyle`
+  button {
+    font-family: 'Roboto';
+  }
+
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <div className="App">
+        {/* <img src={logo} alt="logo" className='App-logo' /> */}
+        <AnimatedLogo src={logo}/>
+        {/* <button>Button</button> */}
+        <StyledButton>Styled Button</StyledButton>
+        <div>
+          <br />
+        </div>
+        <StyledButton variant='outline'>Styled Button</StyledButton>
+        <div>
+          <br />
+        </div>
+        <FancyButton as='a'>Fancy Button</FancyButton>
+        <div>
+          <br />
+        </div>
+        <SubmitButtom as='a'>Fancy Button</SubmitButtom>
+        <div>
+          <br />
+        </div>
+        <DarkButton>Dark Button</DarkButton>
+      </div>
+    </ThemeProvider>
   );
+  
 }
 
 export default App;
---
+++ src/components/Button/Button.js
@@ -0,0 +1,4 @@
+import { StyledButton, FancyButton, SubmitButtom } from "./Button.styles";
+
+export default StyledButton
+export { FancyButton, SubmitButtom }
